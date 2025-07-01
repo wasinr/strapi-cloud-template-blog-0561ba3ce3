1d790a62fe48ab1865ced0b921dad05aeff44b6c
wasin <wasin@digitalmedia.co.th>
2025-07-01 17:24:06 +0700

    get article by slug

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,4 +6,26 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+
+    const entity = await strapi.entityService.findMany('api::article.article', {
+      filters: { slug: slug },
+      populate: {
+        blocks: true,
+        author: true,
+        category: true,
+        cover: true
+      }
+    });
+
+    if (!entity || entity.length === 0) {
+      return ctx.notFound('Article not found');
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity[0], ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
